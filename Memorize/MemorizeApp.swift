1145f2527d4dfc4ee20b0468c5de94fd7643a0e1
Marcis Zoldners <marcis.zoldners@gmail.com>
2021-05-02 21:53:31 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marcis Zoldners on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
